71b85295a92ab96fa9b7c9ae5ddf3d378059fa9b
Pavel Shevchuk <stlwrt@gmail.com>
2023-11-22 00:31:08 +0200

    index.json modification time checker crashed if file was not present

--- download-products.py
+++ download-products.py
@@ -17,15 +17,6 @@ index_file_name = "index.json"
 index_file_path = os.path.join(folder, index_file_name)
 
 
-# Check index file age
-INDEX_FILE_MAX_AGE_MINUTES = 60
-index_file_modification_timestamp = os.path.getmtime(index_file_path)
-current_timestamp = time.time()
-index_file_age_seconds = current_timestamp - index_file_modification_timestamp
-index_file_age_minutes = math.floor(index_file_age_seconds / 60)
-print(f"Index file is {index_file_age_minutes} minutes old")
-
-
 def download_index():
     print("Downloading first page...")
     # https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
@@ -49,15 +40,27 @@ def download_index():
 
 
 # Update inventory index if necessary
-if index_file_age_minutes > INDEX_FILE_MAX_AGE_MINUTES:
+if not os.path.isfile(index_file_path):
     download_index()
 else:
     try:
-        print(f"Loading inventory from {index_file_name}")
-        index_file = open(index_file_path)
-        products = json.load(index_file)
-        print(f"Loaded {len(products)} products from {index_file_path}")
+        # Check index file age
+        INDEX_FILE_MAX_AGE_MINUTES = 60
+        index_file_modification_timestamp = os.path.getmtime(index_file_path)
+        current_timestamp = time.time()
+        index_file_age_seconds = current_timestamp - index_file_modification_timestamp
+        index_file_age_minutes = math.floor(index_file_age_seconds / 60)
+        print(f"Index file is {index_file_age_minutes} minutes old")
+
+        if index_file_age_minutes < INDEX_FILE_MAX_AGE_MINUTES:
+            print(f"Loading inventory from {index_file_name}")
+            index_file = open(index_file_path)
+            products = json.load(index_file)
+            print(f"Loaded {len(products)} products from {index_file_path}")
+        else:
+            download_index()
     except:
+        print(f"Failed to parse index file, redownloading")
         download_index()
 
 
