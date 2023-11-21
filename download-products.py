d41efd844ff7ddf8a5fd31b4bba0ca7074cc61b9
Pavel Shevchuk <stlwrt@gmail.com>
2023-11-21 22:12:23 +0200

    Oops, Python style convention is snake_case, not camelCase

--- download-products.py
+++ download-products.py
@@ -7,22 +7,22 @@ products = []
 # https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
 print("Downloading first page...")
 r = requests.get('https://veikals.banknote.lv/lv/filter-products', params={'categories': 8, 'page': 1})
-firstPage = r.json()
-products.extend(firstPage['data'])
+first_page = r.json()
+products.extend(first_page['data'])
 
-lastPageNumber = firstPage['last_page']
-print(f"Total amount of pages: {lastPageNumber}")
+last_page_number = first_page['last_page']
+print(f"Total amount of pages: {last_page_number}")
 
-for pageNumber in range(2, lastPageNumber+1):
-    print(f"Downloading page #{pageNumber}")
-    r = requests.get('https://veikals.banknote.lv/lv/filter-products', params={'categories': 8, 'page': pageNumber})
-    extraPage = r.json()
-    products.extend(extraPage['data'])
+for page_number in range(2, last_page_number+1):
+    print(f"Downloading page #{page_number}")
+    r = requests.get('https://veikals.banknote.lv/lv/filter-products', params={'categories': 8, 'page': page_number})
+    extra_page = r.json()
+    products.extend(extra_page['data'])
 
 # https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
 folder = "inventory"
-fileName = "index.json"
-filePath = os.path.join(folder, fileName)
-print(f"Dumping {len(products)} products to {filePath}")
-with open(filePath, "w") as outfile:
+file_name = "index.json"
+file_path = os.path.join(folder, file_name)
+print(f"Dumping {len(products)} products to {file_path}")
+with open(file_path, "w") as outfile:
     json.dump(products, outfile)
