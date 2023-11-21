7010b736f72764d0c47239b7c9e32ccf5993d38d
Pavel Shevchuk <stlwrt@gmail.com>
2023-11-21 22:59:21 +0200

    Redownload inventory index if local cache is corrupted

--- download-products.py
+++ download-products.py
@@ -25,8 +25,7 @@ index_file_age_minutes = math.floor(index_file_age_seconds / 60)
 print(f"Index file is {index_file_age_minutes} minutes old")
 
 
-# Update inventory index if necessary
-if index_file_age_minutes > INDEX_FILE_MAX_AGE_MINUTES:
+def download_index():
     print("Downloading first page...")
     # https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
     r = requests.get('https://veikals.banknote.lv/lv/filter-products', params={'categories': 8, 'page': 1})
@@ -45,10 +44,19 @@ if index_file_age_minutes > INDEX_FILE_MAX_AGE_MINUTES:
     # https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
     print(f"Dumping {len(products)} products to {index_file_path}")
     with open(index_file_path, "w") as index_file:
-        json.dump(products, index_file)
+        json.dump(products, index_file, indent=2)
+
+
+# Update inventory index if necessary
+if index_file_age_minutes > INDEX_FILE_MAX_AGE_MINUTES:
+    download_index()
 else:
-    print(f"Loading inventory from {index_file_name}")
-    index_file = open(index_file_path)
-    products = json.load(index_file)
-    print(f"Loaded {len(products)} products from {index_file_path}")
+    try:
+        print(f"Loading inventory from {index_file_name}")
+        index_file = open(index_file_path)
+        products = json.load(index_file)
+        print(f"Loaded {len(products)} products from {index_file_path}")
+    except:
+        download_index()
+
 
