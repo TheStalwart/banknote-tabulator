a6fa42ccbed0d3cdd6f17c0af337700c20bf1160
Pavel Shevchuk <pavels.shevchuks@vialet.eu>
2023-12-27 06:35:12 +0200

    Introduced Banknote.log_tag for log message prefix consistency

--- banknote.py
+++ banknote.py
@@ -34,8 +34,9 @@ class Banknote:
             legacy_file_datetime = datetime.fromtimestamp(legacy_file_timestamp, tz=pytz.timezone('GMT'))
 
             if time.time() - legacy_file_timestamp > 60 * 60 * 24 * 30: # if older than 30 days
-                print(f"[Banknote] Deleting legacy file {legacy_file_path} timestamped {legacy_file_datetime}")
+                print(f"{self.log_tag} Deleting legacy file {legacy_file_path} timestamped {legacy_file_datetime}")
                 os.remove(legacy_file_path)
 
     def __init__(self, path):
         self.path = path
+        self.log_tag = '[Banknote]'
