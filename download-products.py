5010a731f1df3835f47a04b9a434ef31ba20d394
Pavel Shevchuk <stlwrt@gmail.com>
2026-03-16 19:36:13 +0200

    banknote_client is not yet defined when reporting "Another instance of the script is running" to Sentry

--- download-products.py
+++ download-products.py
@@ -41,7 +41,7 @@ except:
 def report_failure_and_exit():
     if betterstack_heartbeat_url:
         print(f"Reporting heartbeat to {betterstack_heartbeat_url}/fail")
-        response = banknote_client.get(f"{betterstack_heartbeat_url}/fail")
+        response = requests.get(f"{betterstack_heartbeat_url}/fail")
         if not response.ok:
             print(f"Failed!")
         print(f"Response: [{response.status_code}]")
